9c460714dd87c3d57c3d1efef7741a789f60ad3f
MomenMustafa45 <momenkara2@gmail.com>
2024-07-23 22:35:46 +0300

    fix: reduced the marin

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
       <div className="container flex flex-col h-full">
         <Navbar />
         <div
-          className="  w-1/2 flex flex-col justify-center mr-auto flex-1"
+          className=" w-3/4 md:w-1/2 flex flex-col justify-center mr-auto flex-1"
           data-aos="fade-right"
         >
           <h1 className="text-white text-2xl md:text-4xl md:leading-[3.5rem]">
@@ -52,7 +52,7 @@ const Hero = () => {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
             fill="#fff"
-            fill-opacity="1"
+            fillOpacity="1"
             d="M0,192L34.3,213.3C68.6,235,137,277,206,250.7C274.3,224,343,128,411,112C480,96,549,160,617,197.3C685.7,235,754,245,823,250.7C891.4,256,960,256,1029,240C1097.1,224,1166,192,1234,197.3C1302.9,203,1371,245,1406,266.7L1440,288L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"
           ></path>
         </svg>
